4b0c1d605c99fe91080c4d35a0b068a3bd26457c
Lucas Malvasia <lucas.malvasia@gmail.com>
2018-10-24 21:45:06 -0300

    project refactor

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,16 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from '../../logo.svg';
 import './App.css';
+import Login from './login/Login';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <a>
-            HELLO!
-          </a>
-        </header>
+        <Login></Login>
       </div>
     );
   }
